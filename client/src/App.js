7d43cebf602a4c8f02822ed9fa6cab7dd05447b6
Jason Lee <lcycug@live.com>
2019-03-11 22:02:39 +0800

    Filters

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,10 @@ import {
 import Spinner from "./components/Spinner";
 
 function App(props) {
+  const tabs = ["All", "Active", "Completed"];
   const [targetId, setTargetId] = useState("");
   const [text, setText] = useState("");
+  const [currentTab, setCurrentTab] = useState("All");
   useEffect(() => {
     props.getTodos();
   }, []);
@@ -47,10 +49,28 @@ function App(props) {
   const handleSwitch = event => {
     props.switchStatus(event.target.id);
   };
+  const handleCurrentTab = event => {
+    setCurrentTab(event.target.id);
+  };
   return (
     <div className="container mx-auto max-w-md">
       <h1 className="text-center text-grey-darkest m-2 p-1">Todo App</h1>
-
+      <div className="text-center">
+        <ul className="list-reset inline-flex">
+          {tabs.map(tab => (
+            <li
+              key={tab}
+              id={tab}
+              className={`p-3 m-3 cursor-pointer ${
+                tab === currentTab ? "font-black" : "font-hairline"
+              }`}
+              onClick={handleCurrentTab}
+            >
+              {tab}
+            </li>
+          ))}
+        </ul>
+      </div>
       {Object.keys(todos).length === 0 ? (
         <Spinner />
       ) : (
@@ -73,44 +93,52 @@ function App(props) {
             </div>
           </form>
           <ul className="block text-center mt-2">
-            {todos.map(todo => (
-              <li
-                key={todo._id}
-                id={todo._id}
-                onDoubleClick={handleSwitch}
-                className={`list-reset rounded-lg py-3 my-2 border border-grey cursor-pointer select-none ${
-                  todo.completed
-                    ? "italic line-through font-hairline"
-                    : "font-black"
-                } ${
-                  targetId === todo._id
-                    ? "bg-blue text-white"
-                    : "bg-transparent hover:bg-blue text-blue-dark hover:text-white"
-                }`}
-              >
-                <i
+            {todos
+              .filter(todo =>
+                currentTab === "All"
+                  ? todo
+                  : currentTab === "Active"
+                  ? todo.completed === false
+                  : todo.completed === true
+              )
+              .map(todo => (
+                <li
+                  key={todo._id}
                   id={todo._id}
-                  onClick={handleSwitch}
-                  className={`far float-left ml-5 ${
-                    todo.completed ? "fa-check-square" : "fa-square"
+                  onDoubleClick={handleSwitch}
+                  className={`list-reset rounded-lg py-3 my-2 border border-grey cursor-pointer select-none ${
+                    todo.completed
+                      ? "italic line-through font-hairline"
+                      : "font-black"
+                  } ${
+                    targetId === todo._id
+                      ? "bg-blue text-white"
+                      : "bg-transparent hover:bg-blue text-blue-dark hover:text-white"
                   }`}
-                />
-                {todo.text}
-                <span className="float-right">
+                >
                   <i
                     id={todo._id}
-                    value={todo.text}
-                    className="far fa-edit pr-5 text-green-light"
-                    onClick={handleEdit}
+                    onClick={handleSwitch}
+                    className={`far float-left ml-5 ${
+                      todo.completed ? "fa-check-square" : "fa-square"
+                    }`}
                   />
-                  <i
-                    id={todo._id}
-                    className="far fa-trash-alt pr-5 text-red-light"
-                    onClick={handleDelete}
-                  />
-                </span>
-              </li>
-            ))}
+                  {todo.text}
+                  <span className="float-right">
+                    <i
+                      id={todo._id}
+                      value={todo.text}
+                      className="far fa-edit pr-5 text-green-light"
+                      onClick={handleEdit}
+                    />
+                    <i
+                      id={todo._id}
+                      className="far fa-trash-alt pr-5 text-red-light"
+                      onClick={handleDelete}
+                    />
+                  </span>
+                </li>
+              ))}
           </ul>
         </>
       )}
